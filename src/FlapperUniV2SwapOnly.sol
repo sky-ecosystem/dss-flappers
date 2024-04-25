a0675d402f74d61afa4c3afb63ea104ec60ae0f1
sunbreak1211 <sunbreak1211@proton.me>
2024-04-25 18:46:52 -0300

    Use solc 0.8.21 + upgrade dss-tests and fix warnings due to it

--- src/FlapperUniV2SwapOnly.sol
+++ src/FlapperUniV2SwapOnly.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.21;
 
 interface SpotterLike {
     function par() external view returns (uint256);
