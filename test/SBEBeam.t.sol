28593edc8289620cea531e52f2bc202482d785ae
sunbreak1211 <sunbreak1211@proton.me>
2026-06-25 08:37:28 -0300

    Fix revert message

--- test/SBEBeam.t.sol
+++ test/SBEBeam.t.sol
@@ -567,7 +567,7 @@ contract SBEBeamTest is DssTest {
             abi.encode(address(0xBAD))
         );
 
-        vm.expectRevert("SBEBeam farm owner mismatch");
+        vm.expectRevert("Farm owner mismatch");
         this.initSBEBeamExt(address(beam), address(farmOwner));
     }
 }
